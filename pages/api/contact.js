af075e470c410ec7540c403fb119b96b53a2fbbd
Anastasiyaaaa <anastasiya.onishchenk@gmail.com>
2023-10-20 19:07:24 +0300

    upd api/contact for Vercel rule

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,24 +1,35 @@
-import {setContact} from "../../helpers/api-util";
+// import {setContact} from "../../helpers/api-util";
 
 async function handler(req, res) {
   const data = req.body.data;
+  console.log(data)
   if (!data) {
     return res.status(400).json({message: "Data is wrong"})
   }
 
   if (req.method === 'POST') {
-    const newData = {
-      _id: new Date().toISOString(),
-      ...data,
-    };
+    // const newData = {
+    //   _id: new Date().toISOString(),
+    //   ...data,
+    // };
     try {
-      const result = await setContact('portfolio', 'contact', newData);
+      // free version of Vercel has limit time for responds, and it's not enough for mongodb
+      // const result = await setContact('portfolio', 'contact', newData);
+
+      const result = await fetch(`${process.env.NEXT_PUBLIC_FIREBASE_HOST}/contact.json`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
 
       res.status(201).json({message: 'All done!', data: result});
     } catch (error) {
       return res.status(400).json({message: error.message})
     }
-
+    console.log('time')
+    console.timeLog();
   }
 }
 
