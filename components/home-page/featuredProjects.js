5768eb5ddbdfa8ce95708e02bb71b534b5bd0714
Anastasiyaaaa <anastasiya.onishchenk@gmail.com>
2023-10-23 09:55:32 +0300

    upd version

--- components/home-page/featuredProjects.js
+++ components/home-page/featuredProjects.js
@@ -1,10 +1,14 @@
 import classes from './featured-projects.module.css';
 import ProjectsGrid from "../projects/projects-grid";
+import Button from "../ui/button";
 
 function FeaturedProjects({projects}) {
   return <section className={classes.latest}>
     <h2>Experience with React</h2>
     <ProjectsGrid projects={projects}/>
+    <div style={{paddingTop: '20px', textAlign: 'center'}}>
+      <Button link='/projects'>Let's check all projects</Button>
+    </div>
   </section>
 }
 
--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -2,13 +2,19 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import classes from './project-item.module.css';
+import {useEffect, useState} from "react";
+
 
 function ProjectItem({project}) {
   const {title, image, excerpt, date, slug} = project;
-
+  const [windowWidth, setWindowWidth] = useState()
   const sliceExcerpt = excerpt.split(' ').slice(0, 23).join(' ') + '...';
 
   const imagePath = `/images/projects/${image}`;
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+  }, [])
 
   return (
     <li className={classes.project}>
@@ -25,6 +31,9 @@ function ProjectItem({project}) {
           <h3>{title}</h3>
           <time>{date}</time>
           <p>{sliceExcerpt}</p>
+          { windowWidth < 780 &&
+            <p style={{color: '#d5bdfc'}}>Click me to read more</p>
+          }
         </div>
       </Link>
     </li>
